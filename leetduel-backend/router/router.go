1630d39c45d3cb0e294408089eb66ee368af1ec5
helloween <yde773786@gmail.com>
2025-06-23 08:59:48 -0400

    firebase backend auth setup

--- leetduel-backend/router/router.go
+++ leetduel-backend/router/router.go
@@ -3,8 +3,17 @@ package router
 import (
 	"net/http"
 	"leetduel-backend/controller"
+	"firebase.google.com/go/v4/auth"
 )
 
-func RegisterRoutes(mux *http.ServeMux){
-	mux.HandleFunc("question/getQuestion", controller.GetRandomQuestion)
+// Required callback when passed auth client
+func authWrapper (authClient *auth.Client, fn func(http.ResponseWriter, *http.Request, *auth.Client)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r, authClient)
+	}
 }
+
+	func RegisterRoutes(mux *http.ServeMux, authClient *auth.Client) {
+		mux.HandleFunc("/question/getQuestion", controller.GetRandomQuestion)
+		mux.HandleFunc("/auth/getUser", authWrapper(authClient, controller.GetUser))
+	}
